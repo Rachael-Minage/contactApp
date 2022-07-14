04a91f62e1cf37b36a730087e3461e734571e911
Rachael-Minage <lauryneminagge@gmail.com>
2022-07-14 10:06:36 +0300

    made changes to view xml file

--- app/src/main/java/com/example/contactapp/MainActivity.kt
+++ app/src/main/java/com/example/contactapp/MainActivity.kt
@@ -2,14 +2,31 @@ package com.example.contactapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.contactapp.databinding.ActivityMainBinding
 import com.example.contactapp.databinding.ContactListItemBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        displayContacts()
+    }
+
+    fun displayContacts() {
+
+        var contact1=Contacts("Rachael","0768501532","baronotieno@gmail.com","South B","https://i.pinimg.com/236x/25/a1/5a/25a15ab58fbe9da14d7efb85227525f7.jpg")
+        var contact2=Contacts("Lauryne","0768501532","levisodhiambo@gmail.com","Westlands","https://i.pinimg.com/564x/35/e0/a0/35e0a0205018063de1a93dabf717815c.jpg")
+        var contact3=Contacts("Minage","0768501532","sofiabaraza@gmail.com","GreenFields","https://www.pinterest.com/pin/25966135343951982/")
+        var contact4=Contacts("Wangoko","0768501532","whitneywanjira@gmail.com","Epren","https://i.pinimg.com/236x/9c/3a/21/9c3a21be85270c8cfddf903ae9abfc96.jpg")
+        var contact5=Contacts("May","0768501532","floidoketch@gmail.com","Kasarani","https://www.pinterest.com/pin/1046383294644174277/")
+        var contact6=Contacts("Gada","0768501532","ruthnabwire@gmail.com","Doonholm","https://i.pinimg.com/236x/37/21/15/3721156d903fe62ec7d0d09b08ce7761.jpg")
+
+        var contactlist = listOf(contact1,contact2,contact3,contact4,contact5,contact6)
+        var contactsAdapter = ContactAdapter(contactlist)
+        binding.rvContacts.layoutManager = LinearLayoutManager(this)
+        binding.rvContacts.adapter = contactsAdapter
     }
 }
--- app/src/main/java/com/example/contactapp/viewContactActivty.kt
+++ app/src/main/java/com/example/contactapp/viewContactActivty.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.contactapp.databinding.ActivityViewContactActivtyBinding
+import com.squareup.picasso.Picasso
 
 class viewContactActivty : AppCompatActivity() {
     lateinit var binding: ActivityViewContactActivtyBinding
@@ -20,18 +21,17 @@ class viewContactActivty : AppCompatActivity() {
         var phone=extras?.getString("PHONE_NUMBER","")
         var email=extras?.getString("EMAIL","")
         var address=extras?.getString("ADDRESS","")
-        var image= binding.
+        var image= binding.imgAvatarTwo
 
 
 
         Toast.makeText(this,name,Toast.LENGTH_LONG).show()
         Toast.makeText(this,phone,Toast.LENGTH_LONG).show()
-        binding.t.text=name
-        binding.tvPhone1.text=phone
-        binding.tvAddress.text=address
-        binding.tvEmail.text=email
+        binding.tvContactName.text=name
+        binding.tvNum.text=phone
+
         Picasso.get().load(intent.getStringExtra("IMAGE")).into(image)
     }
 }
 
-}
+
